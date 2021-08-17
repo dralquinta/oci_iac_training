bf2a8e7b4f5d59cc59fde671e0809f0e02911b5a
dralquinta <dralquinta@gmail.com>
2021-08-17 15:53:48 +0000

    Updating setup_pivot.sh script to make it more robust

--- Pivot_Setup/setup_pivot.sh
+++ Pivot_Setup/setup_pivot.sh
@@ -1,15 +1,29 @@
-echo "[0/10] setting up .bashrc "
+#!/bin/bash
+#Author: DALQUINT - denny.alquinta@oracle.com
+#Purpose: The following script helps autoconfiguring a Development Staging environment on OCI
+#Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved. 
+if [ "$#" -ne 2 ]; then
+
+echo "Missing arguments. Usage: ./setup_pivot.sh guid email"
+
+else
+
+echo "[0/20] Setting up .bashrc aliases"
 sudo tee -a /home/opc/.bashrc > /dev/null <<'EOF'
+export WORKON_HOME=~/envs
+export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
+export VIRTUALENVWRAPPER_VIRTUALENV_ARGS=' -p /usr/bin/python3 '
+source /usr/local/bin/virtualenvwrapper.sh
 export PATH="/home/opc/Terraform":${PATH}
 alias ocibe="cd /home/opc/REPOS/OCIBE"
 alias ocife="cd /home/opc/REPOS/OCIFE"
 alias k="kubectl"
+alias tfversion='f(){ cd ~/Terraform ; unzip -oqq terraform_"$@"_linux_amd64.zip ; cd - ; terraform -version unset -f f; }; f'
 EOF
+echo -e "[0/20] Done.\n\n"
 
-
-
-echo "[1/10] Customizing Wandisco repository and enabling kubernetes, epel repo "
-sudo tee -a /etc/yum.repos.d/wandisco-git.repo  > /dev/null <<'EOF'
+echo "[1/20] Importing Wandisco Repository for GIT 2.x installation"
+sudo tee -a /etc/yum.repos.d/wandisco-git.repo  > /dev/null <<'EOF'
 [wandisco-git]
 name=Wandisco GIT Repository
 baseurl=http://opensource.wandisco.com/rhel/7/git/$basearch/
@@ -18,10 +32,14 @@ gpgcheck=1
 gpgkey=http://opensource.wandisco.com/RPM-GPG-KEY-WANdisco
 EOF
 sudo rpm --import http://opensource.wandisco.com/RPM-GPG-KEY-WANdisco
+echo -e "[1/20] Done.\n\n"
 
+echo "[2/20] Enabling epel repo"
 sudo sed -i 's/enabled=0/enabled=1/g' /etc/yum.repos.d/oracle-epel-ol7.repo
+echo -e "[2/20] Done.\n\n"
 
-sudo tee -a /etc/yum.repos.d/kubernetes.repo  > /dev/null <<'EOF'
+echo "[3/20] Importing Kubernetes repo for kube python"
+sudo tee -a /etc/yum.repos.d/kubernetes.repo  > /dev/null <<'EOF'
 [kubernetes]
 name=Kubernetes
 baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
@@ -30,47 +48,49 @@ gpgcheck=1
 repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
+echo -e "[3/20] Done.\n\n"
 
 
-echo -e "[1/10] Done.\n\n"
-
-#Utilitary Install
-
-echo "[2/10] Installing basic packages (git, ansible, python, go, kubectl, etc.)"
+echo "[4/20] Installing basic packages (git, ansible, python, go, kubectl, etc.)"
 sudo yum -y install go 
 sudo yum -y install git
 sudo yum -y install ansible
 sudo yum -y install java
 sudo yum -y install python3
 sudo yum -y install kubectl
-sudo yum -y install docker
-sudo usermod -a -G docker $USER
-sudo usermod -a -G docker $USER
-sudo systemctl enable docker.service 
-sudo systemctl start docker.service 
-sudo chmod 666 /var/run/docker.sock
+sudo yum -y install docker-engine
+sudo yum -y install oracle-epel-release-el7
+echo -e "[4/20] Done.\n\n"
 
-#
-# Extra tools (PIP & OCI plugins)
-#
+echo "[5/20] Configuring Docker Engine"
+sudo usermod -a -G docker $USER
+sudo usermod -a -G docker $USER
+sudo systemctl enable docker.service 
+sudo systemctl start docker.service 
+sudo chmod 666 /var/run/docker.sock
+echo -e "[5/20] Done.\n\n"
+
+echo "[6/20] Installing Python PIP and extra modules"
 sudo runuser -l opc -c 'curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py'
 sudo runuser -l opc -c 'python get-pip.py'
 sudo runuser -l opc -c 'pip install "pywinrm>=0.2.2"'
 sudo runuser -l opc -c 'pip install oci'
-sudo runuser -l opc -c 'pip3 install kubernetes'
-echo -e "[2/10] Done.\n\n"
+pip3 install --user opc  jupyterlab
+pip3 install --user opc xlutils
+pip3 install --user opc kubernetes
+sudo pip3 install kubernetes
+sudo pip3 install virtualenv
+sudo pip3 install virtualenvwrapper
 
-echo "[3/10] Update OS"
-sudo yum -y update
-echo -e "[3/10] Done.\n\n"
+echo -e "[6/20] Done.\n\n"
 
-echo "[4/10] Install Terraform"
+echo "[7/20] Download and Install Terraform latest stable release"
 mkdir -p /home/opc/Terraform
-sudo runuser -l opc -c 'wget https://releases.hashicorp.com/terraform/0.14.7/terraform_0.14.7_linux_amd64.zip -P /home/opc/Terraform'
-sudo runuser -l opc -c 'unzip /home/opc/Terraform/terraform_0.14.7_linux_amd64.zip -d /home/opc/Terraform'
-echo -e "[4/10] Done.\n\n"
+sudo runuser -l opc -c 'wget $(echo "https://releases.hashicorp.com/terraform/$(curl -s https://checkpoint-api.hashicorp.com/v1/check/terraform | jq -r -M '.current_version')/terraform_$(curl -s https://checkpoint-api.hashicorp.com/v1/check/terraform | jq -r -M '.current_version')_linux_amd64.zip") -P /home/opc/Terraform'
+sudo runuser -l opc -c 'unzip /home/opc/Terraform/* -d /home/opc/Terraform'
+echo -e "[7/20] Done.\n\n"
 
-echo "[5/10] Initializing AWS credentials"
+echo "[8/20] Initializing AWS credentials"
 sudo mkdir -p /home/opc/.aws
 sudo tee -a /home/opc/.aws/credentials > /dev/null <<'EOF'
 [default]
@@ -78,41 +98,45 @@ aws_access_key_id=
 aws_secret_access_key=
 EOF
 sudo chown opc:opc -R /home/opc/.aws
-sudo chmod 0600  /home/opc/.aws/credentials
-echo -e "[5/10] Done.\n\n"
+sudo chmod 0600  /home/opc/.aws/credentials
+echo -e "[8/20] Done.\n\n"
 
-echo "[6/10] Create hosting directories"
+echo "[9/20] Create hosting directories"
 
 mkdir -p /home/opc/REPOS/OCIFE
 mkdir -p /home/opc/REPOS/OCIBE
 mkdir -p /home/opc/Terraform
 mkdir -p /home/opc/.ssh/OCI_KEYS/API
 mkdir -p /home/opc/.ssh/OCI_KEYS/SSH
-echo -e "[6/10] Done.\n\n"
+echo -e "[9/20] Done.\n\n"
 
-echo "[7/10] Create API Keys"
+echo "[10/20] Create API Keys"
 cd /home/opc/.ssh/OCI_KEYS/API
-openssl genrsa -out ./auto_api_key.pem 2048
-chmod go-rwx ./auto_api_key.pem
-openssl rsa -pubout -in ./auto_api_key.pem -out ./auto_api_key_public.pem
-openssl rsa -pubout -outform DER -in ./auto_api_key.pem | openssl md5 -c
-echo -e "[7/10] Done.\n\n"
+openssl genrsa -out ./auto_api_key.pem 2048
+chmod go-rwx ./auto_api_key.pem
+openssl rsa -pubout -in ./auto_api_key.pem -out ./auto_api_key_public.pem
+openssl rsa -pubout -outform DER -in ./auto_api_key.pem | openssl md5 -c
+echo -e "[10/20] Done.\n\n"
 
-echo "[8/10] Create SSH Keys"
+echo "[11/20] Create SSH Keys"
 cd /home/opc/.ssh/OCI_KEYS/SSH
-ssh-keygen -t rsa -N "" -b 2048 -C "autossh" -f ./auto_ssh_id_rsa
+ssh-keygen -t rsa -N "" -b 2048 -C "autossh" -f ./auto_ssh_id_rsa
 cp auto_ssh_id_rsa ../../id_rsa
-echo -e "[8/10] Done.\n\n"
+echo -e "[11/20] Done.\n\n"
 
-echo "[9/10] Disabling epel repo"
+echo "[12/20] Disabling epel repo"
 sudo sed -i 's/enabled=1/enabled=0/g' /etc/yum.repos.d/oracle-epel-ol7.repo
-echo -e "[9/10] Done.\n\n"
+echo -e "[12/20] Done.\n\n"
 
-echo "[10/10] Installing K9s"
-curl -sS https://webinstall.dev/k9s | bash
-echo -e "[10/10] Done.\n\n"
+echo "[13/20] Installing K9s"
+mkdir -p /home/opc/k9s_installer
+wget https://github.com/derailed/k9s/releases/download/v0.24.7/k9s_Linux_x86_64.tar.gz -P /home/opc/k9s_installer
+tar -xvf /home/opc/k9s_installer/k9* -C /home/opc/k9s_installer
+sudo cp /home/opc/k9s_installer/k9s /usr/sbin
+rm -rf /home/opc/k9s_installer
+echo -e "[13/20] Done.\n\n"
 
-echo "[11/10] Installing OCI-CLI"
+echo "[14/20] Installing OCI-CLI"
 sudo runuser -l opc -c 'mkdir -p /home/opc/oci_cli'
 sudo runuser -l opc -c 'wget https://raw.githubusercontent.com/oracle/oci-cli/master/scripts/install/install.sh'
 sudo runuser -l opc -c 'chmod +x install.sh'
@@ -122,12 +146,42 @@ sudo runuser -l opc -c 'rm -r /home/opc/bin'
 sudo runuser -l opc -c 'mkdir -p /home/opc/.oci'
 sudo runuser -l opc -c 'touch /home/opc/.oci/config'
 sudo runuser -l opc -c 'oci setup repair-file-permissions --file /home/opc/.oci/config'
-echo -e "[11/10] Done.\n\n"
+echo -e "[14/20] Done.\n\n"
 
-echo "[12/10] Disabling firewall"
+echo "[15/20] Disabling firewall"
 sudo systemctl stop firewalld
 sudo systemctl disable firewalld
 sudo systemctl mask --now firewalld
-echo "[12/10] Done"
+echo "[15/20] Done"
+
+echo "[16/20] Installing HELM Client"
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+rm get_helm.sh
+echo -e "[16/20] Done.\n\n"
+
+echo "[17/20] Update OS"
+sudo yum -y update
+echo -e "[17/20] Done.\n\n"
+
+echo "[18/20] Fixing Terminal watch for VSCode"
+echo "fs.inotify.max_user_watches=524288" | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p
+echo "[18/20] Done"
 
 
+echo "[19/20] Fixing Installing Oracle Functions CLI"
+curl -LSs https://raw.githubusercontent.com/fnproject/cli/master/install | sh
+echo "[19/20] Done"
+
+echo "[20/20] Rebooting terminal"
+exec -l $SHELL
+echo -e "[20/20] Done.\n\n"
+
+echo "Setting up GIT username and mail"
+git config --global user.name $1
+git config --global user.email $2
+echo -e "Done.\n\n"
+
+fi
